3465bd7a3d52d2eb62d9c7b1e91a72e73d56c4c6
nilarlwin <nilalwin181014@gmail.com>
2021-09-20 14:20:15 -0700

    ant text

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "crud";
-$conn = mysqli_connect($servername,$username,$password);
-mysqli_select_db($conn, $dbname);
-
-?>
